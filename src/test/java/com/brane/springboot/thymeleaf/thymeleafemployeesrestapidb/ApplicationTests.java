34f90d649e8718f310bfe056924adf7cb1488df0
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-06 19:29:43 +0200

    Created Spring Boot project!

---
+++ src/test/java/com/brane/springboot/thymeleaf/thymeleafemployeesrestapidb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brane.springboot.thymeleaf.thymeleafemployeesrestapidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
