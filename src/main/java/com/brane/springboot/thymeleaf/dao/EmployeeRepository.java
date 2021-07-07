6489e3c89007827dbeb8dd6d60094e6ea29ece60
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-07 11:01:03 +0200

    We created interface EmployeeRepository whitch extends Spring Data JPA interface JpaRepository, because we using here Spring Data JPA.Then we don't need to make all CRD(DAO) methods, because this interface(JpaRepisitory) provides all CRUD methods automatically.We just need to set Entity type(Employee) and primary key(Integer).

---
+++ src/main/java/com/brane/springboot/thymeleaf/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.brane.springboot.thymeleaf.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brane.springboot.thymeleaf.entity.Employee;
+
+//we using spring data JPA to extends JpaRepository interface and then we don't need to make all CRUD(DAO) methods.
+//This interface provides all CRUD method automatically.We just need to set Entity type(Employee) and primary key(Integer).
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
