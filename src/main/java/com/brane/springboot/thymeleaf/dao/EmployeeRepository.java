efdfb9e14165a31a889c164caa35fe70271b254d
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-07 11:07:05 +0200

    This EmployeeRepository is actually DAO layer.

--- src/main/java/com/brane/springboot/thymeleaf/dao/EmployeeRepository.java
+++ src/main/java/com/brane/springboot/thymeleaf/dao/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brane.springboot.thymeleaf.entity.Employee;
 
+//THIS IS DAO LAYER
 //we using spring data JPA to extends JpaRepository interface and then we don't need to make all CRUD(DAO) methods.
 //This interface provides all CRUD method automatically.We just need to set Entity type(Employee) and primary key(Integer).
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
